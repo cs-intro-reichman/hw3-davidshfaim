96642f6f2590bbf4fb2365f855ad41e82dbfec70
David Shfaim <davidshfaim@MacBook-Pro-sl-David.local>
2025-11-18 21:29:17 +0200

    fix fix

--- Algebra.java
+++ Algebra.java
@@ -26,42 +26,104 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int i = 0 ;
+		while ( i < x1 ) {
+			x2++;
+			i++;
+			
+		}
+		return x2;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		int result = x1;
+		while (i < x2) {
+		result--;
+		i++;	
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		int result = 0;
+        while (i < x2) {
+		result= plus(result, x1);
+		i++;
+		}
+		return result ;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
 		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		int resulte = 1;
+		while (i < n) {
+			resulte = times(resulte, x);
+			i++ ;
+		}
+		return resulte;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
-	}
+		
+    int result = 0;
+
+    while (x1 >= x2) {
+        x1 = minus(x1, x2); 
+        result = plus(result, 1);
+    }
+
+    return result;
+}
+		
+		
+	
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
-	}	
+         int q = div(x1, x2);     
+    int tmp = times(x2, q);   
+    int r = minus(x1, tmp);    
+    return r;
+}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		 int low = 0;
+    int high = x;
+
+    while (low <= high) {
+        // mid = (low + high) / 2
+        int sum = plus(low, high);
+        int mid = div(sum, 2);
+
+        // square = mid * mid
+        int square = times(mid, mid);
+
+        if (square == x) {
+            return mid; 
+        }
+
+        if (square < x) {
+            low = plus(mid, 1);
+        } else { 
+            high = minus(mid, 1);
+        }
+    }
+
+    
+    return high;
 }
+	
+	}	  	  
